eb9c0064bb911aa5c375d5c24bcaaacba47f57ea
Antony Natale <43508092+tonytheleg@users.noreply.github.com>
2022-03-22 16:07:33 -0400

    Fio reinit test (#1)
    * commenting extra steps and adding ocm base url check
    
    * converting to client.Client because we already have one...why not use it

--- pkg/upgraders/postupgradestep.go
+++ pkg/upgraders/postupgradestep.go
@@ -5,10 +5,9 @@ import (
 	"fmt"
 
 	"github.com/go-logr/logr"
-	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime/schema"
-	"k8s.io/client-go/dynamic"
-	"k8s.io/client-go/rest"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 var (
@@ -17,16 +16,17 @@ var (
 	reinitAnnotation        = map[string]string{"file-integrity.openshift.io/re-init": ""}
 )
 
-/*
-	ToDo:
-	- Need to find a way to determine if the cluster being upgraded is a FedRAMP cluster from within cluster as this only applies there
-		- information is not available in the UpgradeConfig
-		- is there something in cluster we can get to determine if its FR, since clusterdeployments are on hive only?
-		- something added/in OCM?
-*/
-
 // PostUpgradeProcedures are any misc tasks that are needed to be completed after an upgrade has finished to ensure healthy state
 func (c *clusterUpgrader) PostUpgradeProcedures(ctx context.Context, logger logr.Logger) (bool, error) {
+
+	// FIO is a FedRAMP specific operator, PostUpgradeFIOReInit is only for FedRAMP clusters
+	// Check if this is an FR environment via OCM first
+	//ocmConfig := &corev1.ConfigMap{}
+	//_ = c.client.Get(context.Background(), client.ObjectKey{Namespace: fioNamespace, Name: fioObject}, ocmConfig)
+
+	//if ocmBaseUrl.Host != "api.stage.openshift.com" {
+	//	logger.Info("Non-FedRAMP environment...skipping PostUpgradeFIOReInit ")
+	//}
 	err := c.PostUpgradeFIOReInit(ctx, logger)
 	if err != nil {
 		return false, err
@@ -36,32 +36,22 @@ func (c *clusterUpgrader) PostUpgradeProcedures(ctx context.Context, logger logr
 
 // PostUpgradeFIOReInit reinitializes the AIDE DB in file integrity operator to track file changes due to upgrades
 func (c *clusterUpgrader) PostUpgradeFIOReInit(ctx context.Context, logger logr.Logger) error {
-	config, err := rest.InClusterConfig()
-	if err != nil {
-		return fmt.Errorf("failed to fetch config object for in cluster authentication: %v", err)
-	}
-
-	dynamicClient, err := dynamic.NewForConfig(config)
-	if err != nil {
-		return fmt.Errorf("failed to create dynamic client: %v", err)
-	}
-
-	gvr := schema.GroupVersionResource{
-		Group:    "fileintegrity.openshift.io",
-		Version:  "v1alpha1",
-		Resource: "fileintegrities",
-	}
+	u := &unstructured.Unstructured{}
+	u.SetGroupVersionKind(schema.GroupVersionKind{
+		Group:   "fileintegrity.openshift.io",
+		Kind:    "FileIntegrity",
+		Version: "v1alpha1",
+	})
 
 	logger.Info("Fetching File Integrity for re-initialization")
-	fio, err := dynamicClient.Resource(gvr).Namespace(fioNamespace).Get(context.Background(), fioObject, v1.GetOptions{})
+	err := c.client.Get(context.Background(), client.ObjectKey{Namespace: fioNamespace, Name: fioObject}, u)
 	if err != nil {
 		return fmt.Errorf("failed to fetch file integrity %s in %s namespace: %v", fioObject, fioNamespace, err)
 	}
 
 	logger.Info("Setting re-init annotation")
-	fio.SetAnnotations(reinitAnnotation)
-	_, err = dynamicClient.Resource(gvr).Namespace(fioNamespace).Update(context.Background(), fio, v1.UpdateOptions{})
-
+	u.SetAnnotations(reinitAnnotation)
+	err = c.client.Update(context.Background(), u)
 	if err != nil {
 		logger.Error(err, "Failed to annotate File Integrity object")
 		return err
