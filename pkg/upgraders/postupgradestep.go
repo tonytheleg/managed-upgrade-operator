923fa9335e2061fd3f04a91ca04e29627e7e8968
Antony Natale <anatale@redhat.com>
2022-03-22 08:12:29 -0400

    refactoring to use client-go and add proper logging

--- pkg/upgraders/postupgradestep.go
+++ pkg/upgraders/postupgradestep.go
@@ -2,6 +2,7 @@ package upgraders
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/go-logr/logr"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -10,9 +11,15 @@ import (
 	"k8s.io/client-go/rest"
 )
 
+var (
+	fioNamespace     string = "openshift-file-integrity"
+	fioObject        string = "osd-fileintegrity"
+	reinitAnnotation        = map[string]string{"file-integrity.openshift.io/re-init": ""}
+)
+
 /*
 	ToDo:
-	- Need to find a way to determine if the cluster being upgraded is a FedRAMP cluster fro within cluster as this only applies there
+	- Need to find a way to determine if the cluster being upgraded is a FedRAMP cluster from within cluster as this only applies there
 		- information is not available in the UpgradeConfig
 		- is there something in cluster we can get to determine if its FR, since clusterdeployments are on hive only?
 		- something added/in OCM?
@@ -31,12 +38,12 @@ func (c *clusterUpgrader) PostUpgradeProcedures(ctx context.Context, logger logr
 func (c *clusterUpgrader) PostUpgradeFIOReInit(ctx context.Context, logger logr.Logger) error {
 	config, err := rest.InClusterConfig()
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to fetch config object for in cluster authentication: %v", err)
 	}
 
 	dynamicClient, err := dynamic.NewForConfig(config)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to create dynamic client: %v", err)
 	}
 
 	gvr := schema.GroupVersionResource{
@@ -46,16 +53,14 @@ func (c *clusterUpgrader) PostUpgradeFIOReInit(ctx context.Context, logger logr.
 	}
 
 	logger.Info("Fetching File Integrity for re-initialization")
-	fio, err := dynamicClient.Resource(gvr).Namespace("openshift-file-integrity").Get(context.Background(), "example-fileintegrity", v1.GetOptions{})
+	fio, err := dynamicClient.Resource(gvr).Namespace(fioNamespace).Get(context.Background(), fioObject, v1.GetOptions{})
 	if err != nil {
-		logger.Error(err, "Failed to get File Integrity object")
-		return err
+		return fmt.Errorf("failed to fetch file integrity %s in %s namespace: %v", fioObject, fioNamespace, err)
 	}
 
 	logger.Info("Setting re-init annotation")
-	reinit := map[string]string{"file-integrity.openshift.io/re-init": ""}
-	fio.SetAnnotations(reinit)
-	_, err = dynamicClient.Resource(gvr).Namespace("openshift-file-integrity").Update(context.Background(), fio, v1.UpdateOptions{})
+	fio.SetAnnotations(reinitAnnotation)
+	_, err = dynamicClient.Resource(gvr).Namespace(fioNamespace).Update(context.Background(), fio, v1.UpdateOptions{})
 
 	if err != nil {
 		logger.Error(err, "Failed to annotate File Integrity object")
