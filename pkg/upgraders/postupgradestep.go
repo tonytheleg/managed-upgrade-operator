e88e2003c38fa03b02286073ba901744b2e7a73f
Antony Natale <anatale@redhat.com>
2022-03-25 10:07:41 -0400

    added logic to check OCM url for upgrade

--- pkg/upgraders/postupgradestep.go
+++ pkg/upgraders/postupgradestep.go
@@ -3,10 +3,12 @@ package upgraders
 import (
 	"context"
 	"fmt"
+	"strings"
 
 	"github.com/go-logr/logr"
-	"github.com/openshift/managed-upgrade-operator/pkg/configmanager"
-	"github.com/openshift/managed-upgrade-operator/pkg/specprovider"
+	"github.com/openshift/managed-upgrade-operator/config"
+	"gopkg.in/yaml.v2"
+	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -18,35 +20,40 @@ var (
 	reinitAnnotation        = map[string]string{"file-integrity.openshift.io/re-init": ""}
 )
 
+type configManagerSpec struct {
+	ConfigManager struct {
+		Source     string `yaml:"source"`
+		OcmBaseURL string `yaml:"ocmBaseUrl"`
+	} `yaml:"configManager"`
+}
+
 // PostUpgradeProcedures are any misc tasks that are needed to be completed after an upgrade has finished to ensure healthy state
 func (c *clusterUpgrader) PostUpgradeProcedures(ctx context.Context, logger logr.Logger) (bool, error) {
 
 	// FIO is a FedRAMP specific operator, PostUpgradeFIOReInit is only for FedRAMP clusters
-	// Check if this is an FR environment via OCM first
-
-	/*
-		the upgradeconfigmanager uses a specprovider to pull upgrade config specifications
-		the specprovider interface has multiple implementations depending on the configuration in the configmap
-		which defines where to pull upgrade configs from.
-		if pulling from ocm, an ocmprovider will be used, which is what will then use the
-		base URL for communicating to ocm for the purpose of pulling upgrade policies.
-	*/
-	cmb := configmanager.NewBuilder()
-	spec, err := specprovider.NewBuilder().New(c.client, cmb)
+	// Check if this is an FR environment by looking at the MUO Operator config
+	ocmConfig := &corev1.ConfigMap{}
+	err := c.client.Get(context.TODO(), client.ObjectKey{Namespace: config.OperatorNamespace, Name: config.ConfigMapName}, ocmConfig)
 	if err != nil {
-		return false, err
+		return false, fmt.Errorf("failed to fetch %s config map to parse: %v", config.ConfigMapName, err)
 	}
-	fmt.Println(spec)
-	//ocmConfig := &corev1.ConfigMap{}
-	//_ = c.client.Get(context.Background(), client.ObjectKey{Namespace: fioNamespace, Name: fioObject}, ocmConfig)
 
-	//if ocmBaseUrl.Host != "TENTATIVE-FEDRAMP-OCM-URL" {
-	//	logger.Info("Non-FedRAMP environment...skipping PostUpgradeFIOReInit ")
-	//}
-	// err = c.PostUpgradeFIOReInit(ctx, logger)
-	// if err != nil {
-	// return false, err
-	// }
+	var cm configManagerSpec
+	err = yaml.Unmarshal([]byte(ocmConfig.Data["config.yaml"]), &cm)
+	if err != nil {
+		return false, fmt.Errorf("failed to parse %s config map for OCM URL: %v", config.ConfigMapName, err)
+	}
+
+	if cm.ConfigManager.Source == "OCM" {
+		ocmBaseUrl := strings.TrimPrefix(cm.ConfigManager.OcmBaseURL, "https://")
+		if ocmBaseUrl != "TENTATIVE-FEDRAMP-OCM-URL" {
+			logger.Info("Non-FedRAMP environment...skipping PostUpgradeFIOReInit ")
+		}
+		err = c.PostUpgradeFIOReInit(ctx, logger)
+		if err != nil {
+			return false, err
+		}
+	}
 	return true, err
 }
 
@@ -60,14 +67,14 @@ func (c *clusterUpgrader) PostUpgradeFIOReInit(ctx context.Context, logger logr.
 	})
 
 	logger.Info("Fetching File Integrity for re-initialization")
-	err := c.client.Get(context.Background(), client.ObjectKey{Namespace: fioNamespace, Name: fioObject}, u)
+	err := c.client.Get(context.TODO(), client.ObjectKey{Namespace: fioNamespace, Name: fioObject}, u)
 	if err != nil {
 		return fmt.Errorf("failed to fetch file integrity %s in %s namespace: %v", fioObject, fioNamespace, err)
 	}
 
 	logger.Info("Setting re-init annotation")
 	u.SetAnnotations(reinitAnnotation)
-	err = c.client.Update(context.Background(), u)
+	err = c.client.Update(context.TODO(), u)
 	if err != nil {
 		logger.Error(err, "Failed to annotate File Integrity object")
 		return err
